7787b7265756b97ba85d29644f082299452a35b5
DieAndres <diegoandres.koci@gmail.com>
2022-10-21 00:49:51 -0300

    actualizado

--- src/component/pruebacosas.jsx
+++ src/component/pruebacosas.jsx
@@ -2,19 +2,10 @@ import React, { useState,useEffect } from "react";
 import JsonData from "../MOCK_DATA.json";
 import ReactPaginate from "react-paginate";
 const Prueba = (props) =>{
-  debugger
-  const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
-
-  useEffect(() => {
-    setCalculation(() => count * 2);
-  }, [count]); // <- add the count variable here
-
+  
   return (
     <>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
-      <p>Calculation: {calculation}</p>
+     
     </>
   );
 }
